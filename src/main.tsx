a26456a14ae21b3e40e70c3a1b9d957a2d5fa1a4
Me <m.tuto@protonmail.com>
2024-05-24 11:55:47 +0200

    :sparkles: init store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { store } from '@store/index';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
+import { Provider as StoreProvider } from 'react-redux';
 import App from '@/App';
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </StrictMode>,
 );
