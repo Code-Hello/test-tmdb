c0e31324938465f0e21e08759217472f8c0ad9ba
Me <m.tuto@protonmail.com>
2024-05-24 12:01:37 +0200

    :sparkles: add axios

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
